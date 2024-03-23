e4e8591bcced861fdd10254ea267c62faefd5c51
Niklas Hilpert <niklashilpert@freenet.de>
2024-03-23 16:39:40 +0100

    Made ball bouncy

--- src/Main.kt
+++ src/Main.kt
@@ -1,26 +1,48 @@
+import org.apache.batik.swing.JSVGCanvas
 import java.awt.Color
 import java.awt.Dimension
 import java.awt.Graphics
 import java.awt.Graphics2D
 import java.awt.RenderingHints
+import java.awt.event.KeyEvent
+import java.awt.event.KeyListener
+import java.awt.geom.AffineTransform
+import javax.imageio.ImageIO
 import javax.swing.JFrame
 import javax.swing.JPanel
+import javax.swing.SwingUtilities
 
+var transform: AffineTransform = AffineTransform.getRotateInstance(0.0)
 var ballPanel = object : JPanel() {
+
+
     override fun paintComponent(g: Graphics) {
         super.paintComponent(g)
 
         val g2 = g as Graphics2D
         g2.addRenderingHints(RenderingHints(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON))
 
-        g.color = Color.BLUE
-        g.fillOval(0, 0, 100, 100)
+        g2.drawImage(ImageIO.read((javaClass.getResourceAsStream("Basketball.png"))), transform, null)
     }
 }
 
 val ballFrame = JFrame("Ball")
 
+var canvas = JSVGCanvas().apply {
+    preferredSize = Dimension(100, 100)
+    background = Color(0, 0, 0, 0)
+    loadSVGDocument(javaClass.getResource("/Basketball_plain.svg")?.toString())
+
+    setBounds(0, 0, 100, 100)
+    var t = AffineTransform.getRotateInstance(0.5 * Math.PI, 50.0, 50.0)
+    setRenderingTransform(t)
+
+
+
+}
+
 fun main() {
+
     ballPanel.apply {
         preferredSize = Dimension(100, 100)
         background = Color(0xff, 0xff, 0xff, 0)
@@ -30,8 +52,10 @@ fun main() {
         add(ballPanel)
         isUndecorated = true
         background = Color(0xff, 0xff, 0xff, 0)
+        isAlwaysOnTop = true
         pack()
-        val mouseHandler = MouseHandler(ballFrame)
+
+        val mouseHandler = MouseHandler(ballFrame, ballPanel)
         addMouseListener(mouseHandler)
         addMouseMotionListener(mouseHandler)
     }
--- src/UI.kt
+++ src/UI.kt
@@ -1,19 +1,27 @@
+import org.apache.batik.swing.JSVGCanvas
 import java.awt.GraphicsEnvironment
 import java.awt.Point
 import java.awt.event.*
+import java.awt.geom.AffineTransform
 import javax.swing.JFrame
+import javax.swing.JPanel
 import javax.swing.SwingUtilities
 import javax.swing.Timer
 import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 
-class MouseHandler(val frame: JFrame) : MouseListener, MouseMotionListener {
+class MouseHandler(private val frame: JFrame, private val ballPanel: JPanel) : MouseListener, MouseMotionListener {
+    private var rotation = 0.0
+
     private val FPS = 60
     private val DELAY = 1000 / FPS
 
     private var relX = 0
     private var relY = 0
+    private var veloX = 0.0
+    private var veloY = 0.0
+    private var stayOnGround = false
 
     private var physicsTimer: Timer? = null
     private var posTimer: Timer? = null
@@ -32,6 +40,9 @@ class MouseHandler(val frame: JFrame) : MouseListener, MouseMotionListener {
 
         relX = e.x
         relY = e.y
+        veloX = 0.0
+        veloY = 0.0
+        stayOnGround = false
 
         lastFramePositions[0] = Point(frame.x, frame.y)
         for (i in lastFramePositions.indices) {
@@ -41,7 +52,6 @@ class MouseHandler(val frame: JFrame) : MouseListener, MouseMotionListener {
         posTimer = Timer(DELAY) {
             for (i in lastFramePositions.indices) {
                 if (i == 0) continue
-                //print("Replacing ${lastFramePositions[i]} with ${lastFramePositions[i-1]}")
                 lastFramePositions[i] = lastFramePositions[i-1]
             }
             lastFramePositions[0] = Point(frame.x, frame.y)
@@ -49,8 +59,6 @@ class MouseHandler(val frame: JFrame) : MouseListener, MouseMotionListener {
         posTimer!!.start()
     }
     override fun mouseDragged(e: MouseEvent) {
-        //veloX = e.xOnScreen - (x + relX)
-        //veloY = e.yOnScreen - (y + relY)
         val x = max(min(e.xOnScreen - relX, bounds.x + bounds.width - frame.width), bounds.x)
         val y = max(min(e.yOnScreen - relY, bounds.y + bounds.height - frame.height), bounds.y)
 
@@ -63,60 +71,80 @@ class MouseHandler(val frame: JFrame) : MouseListener, MouseMotionListener {
     override fun mouseReleased(e: MouseEvent) {
         posTimer!!.stop()
 
-        //var croppedPositionCount = min(positionCount, lastFramePositions.size)
-
         val latestPos = lastFramePositions[0]!!
 
         val middlePos = lastFramePositions[lastFramePositions.size / 2]!!
         val lastPos = lastFramePositions[lastFramePositions.size-1]!!
         val referencePos = Point((.5 * (middlePos.x + lastPos.x)).toInt(), (.5 * (middlePos.y + lastPos.y)).toInt())
 
-        var veloX = (latestPos.x - referencePos.x).toDouble()
-        var veloY = (latestPos.y - referencePos.y).toDouble()
-        //println()
-        //println("$latestPos $middlePos, $lastPos, $referencePos")
-
-        //println("$veloX, $veloY")
-
-
+        veloX = (latestPos.x - referencePos.x).toDouble()
+        veloY = (latestPos.y - referencePos.y).toDouble()
 
         physicsTimer = Timer(DELAY) {
-            SwingUtilities.invokeLater {
-                var x = frame.x
-                var y = frame.y
-
-                x += Math.round(veloX).toInt()
-                if (veloX != 0.0) {
-                    veloX = (veloX / abs(veloX)) * max(0.0, abs(veloX) * .9)
-                    //println("${vx / abs(vx)} | ${max(0.0, abs(vx) * .9)} -> $vx")
-                    if (veloX > -.05 && veloX < .05)
-                        veloX = 0.0
-                }
-                val (newX, reboundedX) = wrapRebounding(bounds.width - frame.width, x - bounds.x)
-                if (reboundedX) {
-                    veloX *= -1
-                    x = newX + bounds.x
-                }
-
-                y += Math.round(veloY).toInt()
-                if (veloY != 0.0) {
-                    veloY = (veloY / abs(veloY)) * max(0.0, abs(veloY) * .9)
-                    if (veloY > -.05 && veloY < .05)
-                        veloY = 0.0
-                }
-                val (newY, reboundedY) = wrapRebounding(bounds.height - frame.height, y - bounds.y)
-                if (reboundedY) {
-                    veloY *= -1
-                    y = newY + bounds.y
-                }
-
-
-                //println("Current position: $x, $y")
-                frame.setLocation(x, y)
-            }
-
+            step()
         }
         physicsTimer!!.start()
+    }
+
+
+    fun step() {
+        SwingUtilities.invokeLater {
+            var x = frame.x
+            var y = frame.y
+
+            x += Math.round(veloX).toInt()
+            if (veloX != 0.0) {
+                veloX = (veloX / abs(veloX)) * max(0.0, abs(veloX) * .99)
+                //println("${vx / abs(vx)} | ${max(0.0, abs(vx) * .9)} -> $vx")
+                if (veloX > -.05 && veloX < .05)
+                    veloX = 0.0
+            }
+            val (newX, reboundedX) = wrapRebounding(bounds.width - frame.width, x - bounds.x)
+            if (reboundedX != Rebound.None) {
+                veloX *= -1
+                x = newX + bounds.x
+            }
+
+            val oldY = y + Math.round(veloY).toInt()
+            val (newY, reboundedY) = wrapRebounding(bounds.height - frame.height, oldY - bounds.y)
+
+            if (reboundedY != Rebound.None) {
+                println(reboundedY)
+                if (reboundedY == Rebound.Bottom) {
+                    if (veloY < 2) {
+                        veloY = 0.0
+                        stayOnGround = true
+                    }
+                }
+                veloY *= -.7
+
+
+                if (reboundedY == Rebound.Bottom) {
+                    y = bounds.height - frame.height + bounds.y
+                } else {
+                    y += Math.round(veloY).toInt()
+                }
+
+
+            } else {
+                if (!stayOnGround) veloY += 1.5
+                y += Math.round(veloY).toInt()
+                if (veloY > 0) {
+                    //print("+")
+                } else {
+                    //print("-")
+                }
+            }
+            frame.setLocation(x, y)
+
+            if (veloX < .5 && veloX > -0.5) veloX = 0.0
+
+            transform.rotate((veloX / (2 * Math.PI * 50) * 2 * Math.PI), 50.0, 50.0)
+            ballPanel.repaint()
+
+            //println("$veloX $veloY")
+        }
+
     }
 
 
@@ -125,11 +153,21 @@ class MouseHandler(val frame: JFrame) : MouseListener, MouseMotionListener {
     override fun mouseEntered(p0: MouseEvent?) {}
     override fun mouseExited(p0: MouseEvent?) {}
     override fun mouseMoved(p0: MouseEvent?) {}
+
+
 }
 
-fun wrapRebounding(bound: Int, value: Int): Pair<Int, Boolean> {
+fun wrapRebounding(bound: Int, value: Int): Pair<Int, Rebound> {
     var modulus = value % bound
     if (modulus < 0) modulus += bound
-    val rebounded = value > bound || value < 0
-    return Pair(if (rebounded) { bound - modulus } else { modulus }, rebounded)
+
+    val reboundState = if (value > bound) Rebound.Bottom else if (value < 0) Rebound.Top else Rebound.None
+    return Pair(if (reboundState != Rebound.None) { bound - modulus } else { modulus }, reboundState)
 }
+
+enum class Rebound {
+    None,
+    Top,
+    Bottom
+}
+
