e6919ae8b83d4ba7341c0a387e54392464699d74
Niklas Hilpert <niklashilpert@freenet.de>
2024-04-01 15:46:07 +0200

    Added default screen to Shared.kt

--- src/Shared.kt
+++ src/Shared.kt
@@ -1,10 +1,9 @@
+import java.awt.DisplayMode
 import java.awt.GraphicsEnvironment
 import java.awt.Rectangle
 
 const val ballRadius = 50
-val screenBounds: Rectangle = run {
-    val device = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
-    return@run device.defaultConfiguration.bounds
-}
+val defaultScreen = GraphicsEnvironment.getLocalGraphicsEnvironment().defaultScreenDevice
+val screenBounds: Rectangle = defaultScreen.defaultConfiguration.bounds
 
 val exitBounds = Rectangle(25, 25, 50, 50)
